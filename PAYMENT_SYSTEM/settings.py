6b3274ef4b2c5265a487da5a884f20883a9fece0
Project621c <getinada@gmail.com>
2024-10-15 00:58:25 +0300

    bug fix

--- PAYMENT_SYSTEM/settings.py
+++ PAYMENT_SYSTEM/settings.py
@@ -18,6 +18,7 @@ from kombu import Exchange, Queue
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Quick-start development settings - unsuitable for production
